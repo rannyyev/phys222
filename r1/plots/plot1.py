ff5296f02f0e1ba234d144d82a2aeb01f33c3e8e
Rahmanyaz Annyyev <rannyyev@gmail.com>
2024-03-07 18:39:15 +0300

    Update r1

--- r1/plots/plot1.py
+++ r1/plots/plot1.py
@@ -1,8 +1,8 @@
-import matplotlib.pyplot as plt
+from matplotlib import pyplot as plt
 import numpy as np
 
 wavelengths = [404.6, 435.8, 546.1, 579.1]
-refractive_indices = [1.5357, 1.5243, 1.5144, 1.5111]
+refractive_indices = [1.536, 1.524, 1.514, 1.511]
 
 m, b = np.polyfit(wavelengths, refractive_indices, 1)
 x_fit = np.linspace(400, 600, 100)
@@ -14,6 +14,6 @@ plt.xlabel('Wavelength (nm)')
 plt.ylabel('Refractive index')
 plt.grid()
 plt.legend()
-plt.title('Refractive index(n) vs wavelength(λ)')
+plt.title(r'Refractive index ($n$) versus wavelength ($\lambda$)')
 
-plt.savefig('refractive_index_vs_wavelength.png')
+plt.savefig('f2.png', dpi=300)
--- r1/plots/plot2.py
+++ r1/plots/plot2.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 wavelengths = [404.6, 435.8, 546.1, 579.1]
-refractive_indices = [1.5357, 1.5243, 1.5144, 1.5111]
+refractive_indices = [1.536, 1.524, 1.514, 1.511]
 
 for i in range(len(wavelengths)):
     wavelengths[i] = 1 / wavelengths[i]**2
@@ -14,10 +14,10 @@ x_fit = np.linspace(2*10**-6, 8*10**-6, 100)
 y_fit = m * x_fit + b
 plt.plot(x_fit, y_fit, label='y = {:.2e}x + {:.2f}'.format(m, b))
 
-plt.xlabel('1/λ^2 (nm^-2)')
+plt.xlabel(r'$1 / \lambda^2 \, \text{nm}^{-2}$')
 plt.ylabel('Refractive index')
 plt.grid()
 plt.legend()
-plt.title('Refractive index(n) vs 1/λ^2')
+plt.title(r'Refractive index ($n$) versus $1 / \lambda^2$')
 
-plt.savefig('refractive_index_vs_1_over_lambda_squared.png')
+plt.savefig('f3.png', dpi=300)
