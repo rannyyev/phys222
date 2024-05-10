1cd287c2c968ee8394413789103ac492c13f8952
Rahmanyaz Annyyev <rannyyev@gmail.com>
2024-05-10 13:15:29 +0300

    Update r8

--- r8/plots/plot1.py
+++ r8/plots/plot1.py
@@ -1,29 +1,72 @@
-from matplotlib import pyplot as plt
 import numpy as np
+from matplotlib import pyplot as plt
 
 f = 18
-intensitiesclockwise = [1, 0.45 , 0.04 ,0 ,0 , 0.06, 0.1, 0, 0.02, 0,0,0,0,0,0,0,0,0]
-intensitiescounterclockwise = [1,0.8,0.2,0,0,0.4,0.1,0.12,0.06, 0,0,0,0,0,0,0,0,0]
-angles = [0,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80,85]
+intensities_clockwise = [
+    1,
+    0.45,
+    0.04,
+    0,
+    0,
+    0.06,
+    0.1,
+    0,
+    0.02,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+]
+intensities_counterclockwise = [
+    1,
+    0.8,
+    0.2,
+    0,
+    0,
+    0.4,
+    0.1,
+    0.12,
+    0.06,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+]
+angles = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85]
 
-intensities = intensitiesclockwise[::-1] + intensitiescounterclockwise[1:]
+intensities = intensities_clockwise[::-1] + intensities_counterclockwise[1:]
 angles = [-x for x in angles[::-1]] + angles[1:]
 
-#average clockwise and counterclockwise
-intensitiesaverage = [(intensitiesclockwise[i] + intensitiescounterclockwise[i])/2 for i in range(len(intensitiesclockwise))]
+# average clockwise and counterclockwise
+intensitiesaverage = [
+    (intensities_clockwise[i] + intensities_counterclockwise[i]) / 2
+    for i in range(len(intensities_clockwise))
+]
 intensitiesaverage = intensitiesaverage[::-1] + intensitiesaverage[1:]
 
-#plot sinx/x function
+# plot sinx/x function
 sincx = []
 for i in range(len(angles)):
     if angles[i] == 0:
         sincx.append(1)
     else:
-        sincx.append(np.sin(f*np.radians(angles[i]))/(f*np.radians(angles[i])))
+        sincx.append(np.sin(f * np.radians(angles[i])) / (f * np.radians(angles[i])))
 
 print(len(intensities), len(angles))
-plt.plot(angles, intensitiesaverage, label='average')
+plt.plot(angles, intensitiesaverage, label="average")
 grid = plt.grid(True)
-plt.xlabel('Angle (degrees)')
-plt.ylabel('Intensity')
-plt.savefig('r8/plots/plot1.png')
+plt.xlabel("Angle (degrees)")
+plt.ylabel("Intensity")
+
+plt.savefig("f1.png", dpi=300)
+plt.show()
