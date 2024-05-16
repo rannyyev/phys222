ea0625b87e150f94d6c01d75565d1efe651828ee
Rahmanyaz Annyyev <rannyyev@gmail.com>
2024-05-16 18:52:00 +0300

    May 16, 2024: Update r9.tex

--- r9/plots/p2.py
+++ r9/plots/p2.py
@@ -1,25 +1,36 @@
 import math
+
 import matplotlib.pyplot as plt
 
-theta_i = [20,30,40,50,60,70,80]
-theta_t = [7,10,14,18,23,30,37]
-theta_i_exp = [20,25,30,35,40,45,50,55,60,65,70,75,80]
-I_perpendicular_exp = [43,30,53,39,55,32,85,81,97,72,64,35,45]
-I_parallel_exp = [33,22,25,19,17,9,6,3,6,7,16,14,20]
+plt.rcParams["text.usetex"] = True
 
-#(I_paralel / I_perpendicular)^2  vs theta_i
+theta_i = [20, 30, 40, 50, 60, 70, 80]
+theta_t = [7, 10, 14, 18, 23, 30, 37]
+theta_i_exp = [20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80]
+I_perpendicular_exp = [43, 30, 53, 39, 55, 32, 85, 81, 97, 72, 64, 35, 45]
+I_parallel_exp = [33, 22, 25, 19, 17, 9, 6, 3, 6, 7, 16, 14, 20]
 
-I_parallel_theoretical = [math.tan(math.radians(theta_i[i]-theta_t[i]))/math.tan(math.radians(theta_i[i]+theta_t[i])) for i in range(len(theta_i))]
-I_perpendicular_theoretical = [math.sin(math.radians(theta_i[i]-theta_t[i]))/math.sin(math.radians(theta_i[i]+theta_t[i])) for i in range(len(theta_i))]
+r_parallel = [
+    math.tan(math.radians(theta_i[i] - theta_t[i]))
+    / math.tan(math.radians(theta_i[i] + theta_t[i]))
+    for i in range(len(theta_i))
+]
+r_perpendicular = [
+    math.sin(math.radians(theta_i[i] - theta_t[i]))
+    / math.sin(math.radians(theta_i[i] + theta_t[i]))
+    for i in range(len(theta_i))
+]
 
-y_theoretical = [(I_parallel_theoretical[i]/I_perpendicular_theoretical[i])**2 for i in range(len(theta_i))]
-y_exp = [(I_parallel_exp[i]/I_perpendicular_exp[i])**2 for i in range(len(theta_i_exp))]
-plt.plot(theta_i, y_theoretical, 'ro', label='Theoretical Data')
-plt.plot(theta_i, y_theoretical, 'r')
-plt.plot(theta_i_exp, y_exp, 'bo', label='Experimental Data')
-plt.plot(theta_i_exp, y_exp, 'b')
-plt.xlabel('$\\theta_i$ - Incident Angle (degrees)')
-plt.ylabel('$(I_{parallel}/I_{perpendicular})^2$')
-plt.legend()
-plt.grid()
-plt.savefig('p2.png')
+y_theoretical = [(r_parallel[i] / r_perpendicular[i]) ** 2 for i in range(len(theta_i))]
+y_exp = [(I_parallel_exp[i] / I_perpendicular_exp[i]) for i in range(len(theta_i_exp))]
+plt.plot(theta_i, y_theoretical, "ro", label="Theoretical data")
+plt.plot(theta_i, y_theoretical, "r")
+plt.plot(theta_i_exp, y_exp, "bo", label=r"$\left(I_{\parallel}/I_{\perp}$")
+plt.plot(theta_i_exp, y_exp, "b")
+plt.xlabel(r"$\theta_i$ (degrees)")
+plt.ylabel(r"$\left(I_{\parallel}/I_{\perp}$")
+plt.title(r"Plot of $\left(I_{\parallel}/I_{\perp}\right)^2$ vs. $\theta_i$")
+plt.legend(loc="upper right")
+plt.grid(True)
+plt.show()
+# plt.savefig("p2.png")
